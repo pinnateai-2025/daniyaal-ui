181b6c97a7fccdf128e9cf91dc976316e6d42186
Md Danish Ali <alidanish373@gmail.com>
2025-11-24 17:54:36 +0530

    Updated HeroSection

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,9 +1,21 @@
 import "./HeroSection.css";
+import { useNavigate } from "react-router-dom";
 import { FiArrowRight, FiPlay } from "react-icons/fi";
+import heroBg from "../../assets/hero-bg.jfif";
+import heroVideo from "../../assets/hero-video.mp4";
 
 const HeroSection = () => {
+
+    const navigate = useNavigate();
+
     return (
         <section className="hero-section">
+            <div className="hero-image">
+                <img src={heroBg} alt="" />
+            </div>
+            <video className="hero-video" autoPlay loop muted playsInline>
+                <source src={heroVideo} type="video/mp4" />
+            </video>
             <div className="hero-content">
                 <h1>Luxury <span>Attars</span></h1>
                 <h3>Crafted to Perfection</h3>
@@ -12,10 +24,17 @@ const HeroSection = () => {
                     where heritage meets contemporary elegance.
                 </p>
                 <div className="hero-button">
-                    <button className="collection-btn">
+                    <button
+                        className="collection-btn"
+                        onClick={() => navigate("/products")}
+                    >
                         Explore Collection <FiArrowRight className="btn-icon" />
                     </button>
-                    <button className="our-story-btn">
+
+                    <button
+                        className="our-story-btn"
+                        onClick={() => navigate("/our-story")}
+                    >
                         <FiPlay className="btn-icon" /> Our Story
                     </button>
                 </div>
